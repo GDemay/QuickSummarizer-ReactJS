16a92ff56ec6e6a5510ebd7d13dc396c47c047ed
Guillaume Demay <guillaumedemay@hotmail.fr>
2023-03-22 15:30:37 +0100

    init project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import { Container, Form, Button, FormGroup, Label, Input, Alert, Card, CardBody, CardHeader } from "reactstrap";
+import "./App.css";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [summary, setSummary] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleChange = (e) => {
+    setInputText(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // Check if the inputText is empty
+    if (!inputText.trim()) {
+      setError("Error: Please enter text to summarize.");
+      return;
+    }
+    try {
+    const response = await axios.post(
+      `https://e1jvh3piz8.execute-api.eu-west-3.amazonaws.com/summarize?text_to_summarize=${encodeURIComponent(inputText)}`,
+      {} // Pass an empty object as the request body since the required data is in the query parameter
+    );
+
+      // Log the request payload
+      console.log("Request payload: ", inputText);
+      console.log("Response: ", response.data["Summarize content"] );
+
+      // Access the correct property from the API response
+      setSummary( response.data["Summarize content"] );
+      setError(null);
+    } catch (err) {
+      setError("Error: Could not fetch summary.");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="App">
+      <h1 className="mt-4 mb-4">Text Summarizer</h1>
+      <Card className="mb-4">
+        <CardHeader>Text to Summarize</CardHeader>
+        <CardBody>
+          <Form onSubmit={handleSubmit}>
+            <FormGroup className="Input">
+              <Label for="textToSummarize">Enter your text:</Label>
+              <Input
+                type="textarea"
+                name="text"
+                id="textToSummarize"
+                value={inputText}
+                onChange={handleChange}
+              />
+            </FormGroup>
+            <Button color="primary">Summarize</Button>
+          </Form>
+        </CardBody>
+      </Card>
+      {error && <Alert className="mt-4" color="danger">{error}</Alert>}
+      {summary && (
+        <Card className="mb-4">
+          <CardHeader>Summary</CardHeader>
+          <CardBody>
+            <p>{summary}</p>
+          </CardBody>
+        </Card>
+      )}
+    </Container>
   );
 }
 
